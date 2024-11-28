6eefbcede97c5c482f230607ff5a88c9ab5a958a
FranciscoPrieto42 <francisco.prieto.u@proton.me>
2024-11-28 13:50:22 +0000

    Prueba del hook post-commit

--- wpbook.php
+++ wpbook.php
@@ -2,14 +2,14 @@
  * Plugin Name:       Book Collection
  * Plugin URI:        https://presupon.com/wpbook/
  * Description:       Plugin to manage a book collection.
- * Version:           1.10.3
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2
  * Author:            Francisco Prieto
- * Author URI:        https://presupon.com/
+ * Author URI:        https://presupon.com
  * License:           GPL v3 or later
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
- * Update URI:        https://presupon.com/wpbook/
+ * Update URI:        https://presupon.com/wpbook
  * Text Domain:       wpbook
  * Domain Path:       /languages
  */
