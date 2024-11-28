fd3aabad7fe2fdb36511d2b6c0db37209b4eb732
FranciscoPrieto42 <francisco.prieto.u@proton.me>
2024-11-28 13:38:03 +0000

    plugin header

---
+++ wpbook.php
@@ -0,0 +1,16 @@
+/*
+ * Plugin Name:       Book Collection
+ * Plugin URI:        https://presupon.com/wpbook/
+ * Description:       Plugin to manage a book collection.
+ * Version:           1.10.3
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Francisco Prieto
+ * Author URI:        https://presupon.com/
+ * License:           GPL v3 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:        https://presupon.com/wpbook/
+ * Text Domain:       wpbook
+ * Domain Path:       /languages
+ * Requires Plugins:  
+ */
